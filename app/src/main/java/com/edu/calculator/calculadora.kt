0412920e01031f9628dd7aa2f802e57f411633a6
Thiago Dimon Miranda <thiago.298721@alunosatc.edu.br>
2024-03-05 21:29:49 -0300

    initial commit

---
+++ app/src/main/java/com/edu/calculator/calculadora.kt
@@ -0,0 +1,12 @@
+package com.edu.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class calculadora : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculadora)
+    }
+}
